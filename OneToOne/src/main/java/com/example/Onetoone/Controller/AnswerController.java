0041a06ad428230eab4499bc2aa976fb1ffe2f6c
Asvithakaruna <131333265+Asvithakaruna@users.noreply.github.com>
2023-05-06 10:26:24 +0530

    Delete OneToOne directory

--- OneToOne/src/main/java/com/example/Onetoone/Controller/AnswerController.java
+++
@@ -1,5 +0,0 @@
-package com.example.Onetoone.Controller;
-
-public class AnswerController {
-
-}
--- OneToOne/src/main/java/com/example/Onetoone/Repository/ARepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Onetoone.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Onetoone.Model.AModel;
-
-public interface ARepo extends JpaRepository<AModel, Integer> {
-
-}
